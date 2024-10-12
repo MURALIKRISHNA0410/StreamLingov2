9445a55dbb257337f9965ae6b9e0aa105aa8477a
Murali Krishna <muralikrishnamurakonda04.com>
2024-10-12 20:01:49 +0530

    added stun servers

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const PeerConnection = (function () {
     const config = {
       iceServers: [
         {
-          urls: "stun:stun.l.google.com:19302",
+          urls: ["stun:stun.l.google.com:19302", "stun:stun2.l.google.com:19302","stun:stun3.l.google.com:19302"]
         },
       ],
     };
